739bed64417bacef9b73c49e2a8db11f118e182f
Raman <ramanbhandari164@gmail.com>
2025-01-21 12:17:45 -0500

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 
-sudo pacman -S sway wlroots git base-devel python python-pip make \
-    cargo python-virtualenv pipewire wf-recorder swaybg neovim vim sudo foot \
+sudo pacman -S wlroots git base-devel python python-pip make \
+    cargo python-virtualenv pipewire wf-recorder neovim vim sudo foot \
     dunst libnotify wofi zsh fish starship obs-studio xdg-desktop-portal-wlr \
     xdg-desktop-portal wlsunset playerctl grim slurp wl-clipboard yazi \
     trash-cli metasploit nmap gnu-netcat luarocks rust nodejs npm pnpm yarn \
     python-i3ipc neofetch zathura xdg-utils bluetui imv mpv zoxide pyenv hurl \
     nemo btop hyprland hyprlock hyprpaper hypridle noto-fonts-emoji noto-fonts-extra os-prober grub \
-    wlsunset wev ripgrep fzf cowsay tmux networkmanager aws-cli azure-cli \
+    wev ripgrep fzf cowsay tmux networkmanager aws-cli azure-cli \
     proxychains-ng v2ray github-cli bluez bluez-utils bluetui blueman openvpn cloudflared \
     docker docker-compose wireplumber less tree ttf-iosevka-nerd brightnessctl fd \
     tldr locate go python-pipx glow ltrace cutter radare2 rz-ghidra r2ghidra \
@@ -23,6 +23,10 @@ makepkg -si
 cd ..
 rm -rf paru-bin
 
+# Install additional packages via paru
+paru -S yambar caido-cli burpsuite zen-browser-bin xcp virtualfish waydroid \
+	python-pyclip subfinder httpx
+
 # Clone yay from AUR and install it
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -31,15 +35,15 @@ cd ..
 rm -rf yay
 
 # Install additional packages via yay
-yay -S waybar-module-pacman-updates-git
-
-# Install additional packages via paru
-paru -S yambar caido-cli burpsuite zen-browser-bin xcp virtualfish waydroid \
-	python-pyclip subfinder httpx
+yay -S waybar-module-pacman-updates-git ueberzupp xarchiver
 
 # Clone dotfiles repository and set up .zshrc
 rm -rf ~/.config
 git clone -b main https://github.com/raman164/dotfiles-hyprland.git ~/.config
+cd ~/.config
+mv -f .zshrc weather.sh ~/.
+cd
+
 
 # Add current user to necessary groups
 sudo usermod -aG kvm,video,libvirt,docker,network,input $(whoami)
