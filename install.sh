8844f3f7481f0a735e19133754346fc60716d21f
Raman <ramanbhandari164@gmail.com>
2025-01-28 12:42:25 -0500

    install.sh updated

--- install.sh
+++ install.sh
@@ -1,19 +1,16 @@
 
 
-sudo pacman -S base-devel python3 python-pip make \
-    python-virtualenv neovim foot \
-    dunst wofi zsh fish starship \
-    xdg-desktop-portal-hyprland playerctl grim yazi \
-    luarocks rust nodejs npm \
-    neofetch zathura xdg-utils bluetui imv mpv \
+sudo pacman -S gnu-free-fonts noto-fonts noto-fonts-cjk nerd-fonts make \
+    neovim foot dunst wofi zsh fish starship waybar swww sddm\
+    xdg-desktop-portal-hyprland xdg-desktop-portal-kde playerctl grim yazi \
+    rust nodejs npm neofetch zathura xdg-utils imv mpv grim slurp\
     hyprland hyprlock hyprpaper hypridle noto-fonts-emoji noto-fonts-extra \
-    fzf tmux networkmanager \
-    bluez bluez-utils bluetui blueman \
-    tree ttf-iosevka-nerd brightnessctl \
-    go python-pipx \
-    adwaita-icon-theme \
-	lzip bat eza firefox sddm pamixer zsh-autosuggestions waybar thunar wpctl \
-    xdg-user-dirs tumbler --needed \
+    fzf tmux networkmanager qt5-imageformats ffmpegthumbs taglib kompare ark\
+    bluez bluez-utils bluetui blueman xdg-desktop-portal-gtk qt5-wayland jq\
+    tree ttf-iosevka-nerd brightnessctl pavucontrol nwg-look\
+    adwaita-icon-theme dolphin dolphin-plugins kdegraphics-thumbnailers libheif\
+	lzip bat eza exa firefox sddm pamixer zsh-autosuggestions waybar thunar wpctl \
+    xdg-user-dirs tumbler python3 polkit-kde-agent fuzzel--needed \
         
 
 # Clone yay from AUR and install it
@@ -36,6 +33,7 @@ cd ~/.config
 mv -f .zshrc weather.sh ~/.
 cd
 
+sudo systemctl enable sddm
 
 # Final message
 echo "Package & rice installation completed!"
