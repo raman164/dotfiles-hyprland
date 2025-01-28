87016c43b4485724abf07c62e093e07a8702743d
Raman <ramanbhandari164@gmail.com>
2025-01-28 13:37:09 -0500

    install.sh updated

--- install.sh
+++ install.sh
@@ -1,17 +1,95 @@
+#!/bin/bash
 
+# List of packages to install
+packages=(
+    gnu-free-fonts
+    noto-fonts
+    noto-fonts-cjk
+    nerd-fonts make
+    neovim
+    foot
+    dunst
+    wofi
+    zsh
+    fish
+    starship
+    waybar
+    swww
+    sddm
+    xdg-desktop-portal-hyprland
+    xdg-desktop-portal-kde
+    playerctl
+    grim
+    yazi
+    rust
+    nodejs
+    npm
+    neofetch
+    zathura
+    xdg-utils
+    imv
+    mpv
+    grim
+    slurp
+    hyprland
+    hyprlock
+    hyprpaper
+    hypridle
+    noto-fonts-emoji
+    noto-fonts-extra
+    fzf
+    tmux
+    networkmanager
+    qt5-imageformats
+    ffmpegthumbs
+    taglib
+    kompare
+    ark
+    bluez
+    bluez-utils
+    bluetui
+    blueman
+    xdg-desktop-portal-gtk
+    qt5-wayland
+    jq
+    tree
+    ttf-iosevka-nerd
+    brightnessctl
+    pavucontrol
+    nwg-look
+    adwaita-icon-theme
+    dolphin
+    dolphin-plugins
+    kdegraphics-thumbnailers
+    libheif
+	lzip
+    bat
+    eza
+    exa
+    firefox
+    sddm
+    pamixer
+    zsh-autosuggestions
+    waybar
+    thunar
+    wpctl
+    xdg-user-dirs
+    tumbler
+    python3
+    polkit-kde-agent
+    fuzzel 
+    
+)
 
-sudo pacman -S gnu-free-fonts noto-fonts noto-fonts-cjk nerd-fonts make \
-    neovim foot dunst wofi zsh fish starship waybar swww sddm\
-    xdg-desktop-portal-hyprland xdg-desktop-portal-kde playerctl grim yazi \
-    rust nodejs npm neofetch zathura xdg-utils imv mpv grim slurp\
-    hyprland hyprlock hyprpaper hypridle noto-fonts-emoji noto-fonts-extra \
-    fzf tmux networkmanager qt5-imageformats ffmpegthumbs taglib kompare ark\
-    bluez bluez-utils bluetui blueman xdg-desktop-portal-gtk qt5-wayland jq\
-    tree ttf-iosevka-nerd brightnessctl pavucontrol nwg-look\
-    adwaita-icon-theme dolphin dolphin-plugins kdegraphics-thumbnailers libheif\
-	lzip bat eza exa firefox sddm pamixer zsh-autosuggestions waybar thunar wpctl \
-    xdg-user-dirs tumbler python3 polkit-kde-agent fuzzel--needed \
-        
+# Loop through the list and install each package
+for pkg in "${packages[@]}"; do
+    echo "Attempting to install: $pkg"
+    sudo pacman -S --needed --noconfirm "$pkg"
+    if [ $? -ne 0 ]; then
+        echo "Package '$pkg' not found or failed to install. Skipping..."
+    fi
+done
+
 
 # Clone yay from AUR and install it
 git clone https://aur.archlinux.org/yay.git
